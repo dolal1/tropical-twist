a5930944d4478c8d739f3d2ac0f58e271e266190
David OH <holifeilddavid@gmail.com>
2021-06-16 14:29:31 +0300

    Authentication Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Login from "./components/Login";
 import Checkout from "./components/Checkout";
 import "./App.css";
 
 function App() {
+  const [, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //runs only once when app component loads
+    auth.onAuthStateChanged((authUser) => {
+      console.log("User is >>> ", authUser);
+
+      if (authUser) {
+        //user authenticated
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //user unauthenticated
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    }); // eslint-disable-next-line
+  }, []);
+
   return (
     //BEM
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route path="/" exact>
+            <Header />
             <Home />
           </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
         </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCT1ziLORMPg31Ys2d80RwZwkKjkmuC1Zw",
   authDomain: "tropical-twist.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "215976699784",
   appId: "1:215976699784:web:95089ce064f5409b577c3d",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
