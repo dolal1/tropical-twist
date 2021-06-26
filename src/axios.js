99a52fcb894118290852a10e0c4e359941c2ccef
David OH <holifeilddavid@gmail.com>
2021-06-27 02:06:09 +0300

    Project Complete

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:5001/tropical-twist/us-central1/api",
-  //API(Cloud Function) URL
+  // API(Cloud Function) URL
+  // baseURL is where one would input the url to the backend
 });
 
 export default instance;
